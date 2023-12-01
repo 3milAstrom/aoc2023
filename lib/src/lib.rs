7dfca94f1e99ec7d6fda2a7f7215c37711dc9afa
Emil <emme332spel@gmail.com>
2023-12-01 07:58:02 +0100

    Added part 1

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -4,6 +4,26 @@ pub fn read_file(path: String) -> String {
     fs::read_to_string(path).expect("Should ba able to read file")
 }
 
+pub fn read_lines(filename: &str) -> Vec<String> {
+    fs::read_to_string(filename) 
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
+
+pub fn is_number(s: String) -> bool {
+    let r = s.parse::<i32>();
+    match r {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+pub fn char_is_number(s: &char) -> bool {
+    is_number(s.to_string())
+}
+
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
