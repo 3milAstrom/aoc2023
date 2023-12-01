324ae14de283070c7acf2a8806a0a9c067415efe
emme332spel@gmail.com <emme332spel@gmail.com>
2023-12-01 21:36:58 +0100

    Added day2 template

---
+++ day2/src/bin/part1.rs
@@ -0,0 +1,19 @@
+use lib::*;
+
+fn main() {
+    let sv = read_lines("part1.txt");
+
+    let nv: Vec<String> = sv
+        .into_iter()
+        .map(|x| {
+            let c: Vec<char> = x.chars().collect();
+            let n: Vec<char> = c.into_iter()
+                .filter(|y| char_is_number(y))
+                .collect();
+            vec![*n.first().expect("to exist"), *n.last().expect("to exist")].iter().collect()
+        })
+        .collect();
+    let sum: i32 = nv.iter().map(|f| f.parse::<i32>().expect("to be numbers")).sum();
+
+    println!("Sum: {}", sum) //54597
+}
