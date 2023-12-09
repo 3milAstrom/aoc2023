544a61a0b9d2bdd32ed747efd0255c111eaa3072
Emil <emme332spel@gmail.com>
2023-12-09 08:53:59 +0100

    Remade into trait

--- day9/src/bin/part2.rs
+++ day9/src/bin/part2.rs
@@ -1,36 +1,53 @@
+use std::ops::{Add, Sub};
+
 use lib::*;
 
 pub fn parse_numbers(input: &str) -> Vec<i64> {
-    input.split(" ").map(|c| {
-        c.parse::<i64>().expect("to be valid number")
-    }).collect()
+    input
+        .split(" ")
+        .map(|c| c.parse::<i64>().expect("to be valid number"))
+        .collect()
 }
 
-fn inter_exterpolate(vec: &Vec<i64>) -> i64 {
-    let mut v: Vec<i64> = Vec::with_capacity(vec.len() - 1);
-    for i in 0..(vec.len()) {
-        if i != vec.len() - 1 {
-            v.push(vec[i+1] - vec[i]);
-        }
-    }
+trait Polate<T> {
+    fn inter_exterpolate(vec: &Vec<T>) -> T;
+}
 
-    if !(v.iter().filter(|c| **c == 0).count() == v.len()) {
-        let ans = inter_exterpolate(&v);
-        let current = v.first().expect("to be number");
-        return  current - ans;
-    };
-    v.first().expect("to be a zero").clone()
+impl<T> Polate<T> for Vec<T>
+where
+    T: Sub<Output = T> + Add<Output = T> + Default + Copy + Eq + PartialEq,
+{
+    fn inter_exterpolate(vec: &Vec<T>) -> T {
+        let mut v: Vec<T> = Vec::with_capacity(vec.len() - 1);
+        for i in 0..(vec.len()) {
+            if i != vec.len() - 1 {
+                v.push(vec[i + 1] - vec[i]);
+            }
+        }
+
+        if !(v.iter().filter(|c| **c == Default::default()).count() == v.len()) {
+            let ans = Self::inter_exterpolate(&v);
+            let current = v.first().expect("to be number");
+            return *current - ans;
+        };
+        v.first().expect("to be a zero").clone()
+    }
 }
 
 fn main() {
     let lines = read_lines("part1.txt");
-    let r = lines.iter().map(|l| parse_numbers(l.as_str())).collect::<Vec<Vec<i64>>>();
+    let r = lines
+        .iter()
+        .map(|l| parse_numbers(l.as_str()))
+        .collect::<Vec<Vec<i64>>>();
 
-    let sum: i64 = r.iter().map(|l| {
-        let ans = inter_exterpolate(l);
-        l.first().expect("to exist") - ans
-    }).sum();
+    let sum: i64 = r
+        .iter()
+        .map(|l| {
+            let ans = Vec::inter_exterpolate(l);
+            l.first().expect("to exist") - ans
+        })
+        .sum();
 
     println!("Sum: {}", sum) // 903
-
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -37,23 +37,3 @@ pub fn is_number(s: String) -> bool {
 pub fn char_is_number(s: &char) -> bool {
     is_number(s.to_string())
 }
-
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-pub fn print_hello() -> () {
-    println!("Hello");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
